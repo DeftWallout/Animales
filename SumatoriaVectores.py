3cb52553632e9485d7c1461181a307ca926ae2d1
DeftWallout <lpethlol@gmail.com>
2025-03-21 12:56:27 -0500

    Sumatoria de vectores 2D-3D y sumatoria de vectores complejos o reales ARREGLADO

--- SumatoriaVectores.py
+++ SumatoriaVectores.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from abc import ABC, abstractmethod
+
 
 class Vector(ABC):
     def __init__(self, *components):
@@ -38,32 +40,6 @@ class Vector3D(Vector):
         raise TypeError("Solo se pueden sumar vectores de la misma dimensión")
 
 
-class VectorReal(Vector):
-    def __init__(self, *components):
-        super().__init__(*components)
-
-    def sumar(self, otro):
-        if isinstance(otro, VectorReal):
-            return VectorReal(*[a + b for a, b in zip(self.components, otro.components)])
-        raise TypeError("Solo se pueden sumar vectores reales entre sí")
-
-    def suma_cuadrados(self):
-        return sum(a ** 2 for a in self.components)
-
-
-class VectorComplejo(Vector):
-    def __init__(self, *components):
-        super().__init__(*components)
-
-    def sumar(self, otro):
-        if isinstance(otro, VectorComplejo):
-            return VectorComplejo(*[a + b for a, b in zip(self.components, otro.components)])
-        raise TypeError("Solo se pueden sumar vectores complejos entre sí")
-
-    def suma_cuadrados(self):
-        return sum(abs(a) ** 2 for a in self.components)
-
-
 # Ejemplo de uso
 v1 = Vector2D(1, 2)
 v2 = Vector2D(3, 4)
@@ -73,13 +49,7 @@ v3 = Vector3D(1, 2, 3)
 v4 = Vector3D(4, 5, 6)
 print(v3.sumar(v4))  # Vector3D(5, 7, 9)
 
-vr1 = VectorReal(1.2, 3.4, 5.6)
-vr2 = VectorReal(2.1, 4.3, 6.5)
-print(vr1.sumar(vr2))  # VectorReal(3.3, 7.7, 12.1)
-print(vr1.suma_cuadrados())  # Suma de los cuadrados de los componentes reales
 
-vc1 = VectorComplejo(1 + 2j, 3 + 4j)
-vc2 = VectorComplejo(5 + 6j, 7 + 8j)
-print(vc1.sumar(vc2))  # VectorComplejo((6+8j), (10+12j))
-print(vc1.suma_cuadrados())  # Suma de los cuadrados de los módulos de los números complejos
+
+
 
